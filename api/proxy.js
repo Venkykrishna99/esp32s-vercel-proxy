d5b188ca6d7cb6bf88853edaf856358a295eda3d
krishna <155258579+Venkykrishna99@users.noreply.github.com>
2025-02-16 03:37:24 +0530

    Update proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -2,11 +2,11 @@ import axios from "axios";
 
 export default async function handler(req, res) {
     if (req.method !== "POST") {
-        return res.status(405).json({ error: "Method Not Allowed" });
+        return res.status(405).json({ error: "Method Not Allowed", message: "Only POST requests are allowed" });
     }
 
     try {
-        const TARGET_URL = "http://electronova.infinityfreeapp.com/esp8266/save_data.php"; // Update this!
+        const TARGET_URL = "http://electronova.infinityfreeapp.com/esp8266/save_data.php"; // Change this!
 
         const response = await axios.post(TARGET_URL, req.body, {
             headers: { "Content-Type": "application/x-www-form-urlencoded" },
